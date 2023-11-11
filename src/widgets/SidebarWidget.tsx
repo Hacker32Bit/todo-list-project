c0c8a990038a835a0e7a58002eed22fbcdf15273
Gektor Gevorgyan <gektor.gevorgyan@gmail.com>
2023-11-11 14:20:02 +0400

    Added user card in dashboard sidebar

--- src/widgets/SidebarWidget.tsx
+++ src/widgets/SidebarWidget.tsx
@@ -1,31 +1,46 @@
 import React from "react";
 
-import "./SidebarWidget.css"
+import "./SidebarWidget.css";
 import { Link } from "react-router-dom";
 
 const SidebarWidget: React.FC = () => {
-    return(
-        <div className="sidebar">
-            <div className="profile">
-                Some user information
-            </div>
-            <div className="line"></div>
-            <div className="menu">
-                <ul>
-                    <Link to="#"><li>Board</li></Link>
-                    <Link to="#"><li>Members</li></Link>
-                    <Link to="#"><li>Workspace settings</li></Link>
-                </ul>
-            </div>
-            <div className="line"></div>
-            <div className="menu">
-                <ul>
-                    <Link to="#"><li>Table 1</li></Link>
-                    <Link to="#"><li>Table 2</li></Link>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="sidebar">
+      <div className="profile">
+        <img
+          src="https://fs01.cap.ru//www21-11/galatr/person/cb45deff-7216-4306-80f7-9e48d03f437e/no_avatar_3st4mbc2.png"
+          alt="User"
+        ></img>
+        <h3>Name Surname</h3>
+        <span>Student</span>
+      </div>
+      <div className="line"></div>
+      <div className="menu">
+        <ul>
+          <Link to="#">
+            <li>Board</li>
+          </Link>
+          <Link to="#">
+            <li>Members</li>
+          </Link>
+          <Link to="#">
+            <li>Workspace settings</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="line"></div>
+      <div className="menu">
+        <ul>
+          <Link to="#">
+            <li>Table 1</li>
+          </Link>
+          <Link to="#">
+            <li>Table 2</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default SidebarWidget;
